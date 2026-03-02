76ba9c72869874dbc03f03cde67142fb87d72042
ensEIVA <ens@eiva.com>
2026-03-02 10:26:28 +0100

    feat: update TypeScript configuration and enhance Vite setup with path aliases

--- rotv-flight/vite.config.ts
+++ rotv-flight/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@obr": "@ocean-industries-concept-lab/openbridge-webcomponents-react",
+      "@obc": "@ocean-industries-concept-lab/openbridge-webcomponents/dist",
+      "@": path.resolve(__dirname, "./src"),
+    }
+  }
 })
