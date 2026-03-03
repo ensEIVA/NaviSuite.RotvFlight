fa7ae21f39d3ce6e3b4d8440ddba42442cf6a5b3
ensEIVA <ens@eiva.com>
2026-03-03 21:11:09 +0100

    topbar layout

--- rotv-flight/src/components/Header/Header.tsx
+++ rotv-flight/src/components/Header/Header.tsx
@@ -2,6 +2,13 @@ import { useState, useEffect } from "react";
 import "./Header.css";
 import { ObcTopBar } from "@ocean-industries-concept-lab/openbridge-webcomponents-react/components/top-bar/top-bar";
 import { ObiShip } from "@ocean-industries-concept-lab/openbridge-webcomponents-react/icons/icon-ship";
+import { ObiCloseGoogle } from "@ocean-industries-concept-lab/openbridge-webcomponents-react/icons/icon-close-google";
+import {
+  ObcBrillianceMenu,
+  type ObcPaletteChangeEvent,
+} from "@ocean-industries-concept-lab/openbridge-webcomponents-react/components/brilliance-menu/brilliance-menu";
+import { ObcPalette } from "@ocean-industries-concept-lab/openbridge-webcomponents/dist/components/brilliance-menu/brilliance-menu";
+import { ObcTopBarMenuButtonIcon } from "@ocean-industries-concept-lab/openbridge-webcomponents/dist/components/top-bar/top-bar";
 // ---------------------------------------------------------------------------
 // Topbar — OpenBridge light theme
 //
@@ -13,21 +20,47 @@ import { ObiShip } from "@ocean-industries-concept-lab/openbridge-webcomponents-
 
 export function Header() {
   const [utcTime, setUtcTime] = useState<string>(() => formatUtc(new Date()));
+  const [showBrillianceMenu, setShowBrillianceMenu] = useState(false);
+  const [palette, setPalette] = useState(ObcPalette.day);
+
+  const handleDimmingButtonClicked = () => {
+    setShowBrillianceMenu(!showBrillianceMenu);
+  };
 
   useEffect(() => {
     const id = setInterval(() => setUtcTime(formatUtc(new Date())), 1000);
     return () => clearInterval(id);
   }, []);
 
+  const handlePalleteChange = (e: ObcPaletteChangeEvent) => {
+    document.documentElement.setAttribute("data-obc-theme", e.detail.value);
+    setPalette(e.detail.value);
+  };
+
   return (
-    <ObcTopBar
-      showAppIcon
-      showAppsButton
-      showDimmingButton
-      pageName="ROTV-Flight"
-    >
-      <ObiShip slot="app-icon"></ObiShip>
-    </ObcTopBar>
+    <>
+      {showBrillianceMenu && (
+        <ObcBrillianceMenu
+          onPaletteChanged={handlePalleteChange}
+          palette={palette}
+          hideBrightness
+          className="brilliance"
+        />
+      )}
+      <ObcTopBar
+        showAppIcon
+        showDimmingButton
+        menuButtonIcon={ObcTopBarMenuButtonIcon.Home}
+        dimmingButtonActivated={showBrillianceMenu}
+        onDimmingButtonClicked={handleDimmingButtonClicked}
+        
+        appTitle="ROTV-Flight"
+        showUserButton
+      >
+        
+        <ObiShip slot="app-icon"></ObiShip>
+      </ObcTopBar>
+    </>
   );
 }
 
